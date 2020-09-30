f48c0cf9f0fb6f83570353e8bbebe637900df0bb
kelvin9314 <kelvin9314@gmail.com>
2020-09-30 15:11:25 +0800

    stage-three: Nginx reverse proxy to nextjs anf nuxtjs (two internal container)

---
+++ stage-three/nuxtjs/pages/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <main>
+    <h1>Home page</h1>
+     <nuxt-link to="/"> Index (internal link that belongs to the Nuxt App)</nuxt-link>
+    <a href="https://nuxtjs.org">External Link to another page</a>
+  </main>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
